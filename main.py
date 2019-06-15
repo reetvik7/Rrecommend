9ba0681e4c35baa6b0e70fb7a7c390360648cfc9
reetvik7 <51454104+reetvik7@users.noreply.github.com>
2019-06-16 00:01:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,38 @@ dat = pd.read_csv("dataset/refined_dataset.csv")
 data = pd.DataFrame(dat)
 
 print("Welcome....")
-n = int(input(print("Choose the criteria on which you want your recommendations to be:\n1.Year\n2.Runtime\n3.Genre\n4.director\n5.cast\n6.Movie")))
+n = int(input(print("Choose the criteria on which you want your recommendations to be:\n1.Movie\n2.Runtime\n3.Genre\n4.director\n5.cast\n6.Year")))
 
 if n == 1:
-    p = int(input(print("Enter the years from which you want the movies to be")))
-    x=int(input(print("how many recommendations do you want")))
-    res = pd.merge(data, year(p), on='movieId')
-    res = res.sort_values(by=['weighted score'], ascending=[False])
-    print(res[["original_title", "year_x"]].head(x))
+    r_cols = ['user_id', 'movie_id', 'rating']
+    rating = pd.read_csv('dataset/ratings.csv', names=r_cols)
+
+    df = pd.read_csv('dataset/final.csv')
+    df_title = df[["movie_id", "title"]]
+    df_title.set_index('movie_id', inplace=True)
+
+    ratings = pd.merge(df_title, rating, on="movie_id")
+    df_p = ratings.pivot_table(index=['user_id'], columns=['movie_id'], values='rating').reset_index(drop=True)
+    df_p.fillna(0, inplace=True)
+
+    df_title['check'] = df_title["title"].str.lower().str.replace(" ", "")
+    df_title["check"] = df_title['check'].map(lambda x: re.sub(r'\W+', '', x))
+
+    inp = str(input("Enter Movie\n"))
+    inp = inp.lower()
+    inp = inp.replace(" ", "")
+    movie = df_title[df_title['check'].str.contains(inp)]
+    x = movie.values
+    print("The following movies matched your search")
+    print('########################################')
+    for i in range(len(x)):
+        print(str(i + 1) + " " + str(x[i][0]))
+    user = int(input("\nPlease select a movie by entering its index number\n"))
+    movie_title = x[user - 1][0]
+    n = int(input("how many recommendations do you want??\n"))
+    recommendations = recommend(movie_title, n)
+    print(recommendations)
+
 
 if n == 2:
     p = int(input(print("Enter the runtime of your movie in minutes")))
@@ -65,31 +89,9 @@ if n == 5:
     print(res[["original_title","cast_x", "year"]].head(x))
 
 if n == 6:
-    r_cols = ['user_id', 'movie_id', 'rating']
-    rating = pd.read_csv('dataset/ratings.csv', names=r_cols)
+    p = int(input(print("Enter the years from which you want the movies to be")))
+    x = int(input(print("how many recommendations do you want")))
+    res = pd.merge(data, year(p), on='movieId')
+    res = res.sort_values(by=['weighted score'], ascending=[False])
+    print(res[["original_title", "year_x"]].head(x))
 
-    df = pd.read_csv('dataset/final.csv')
-    df_title = df[["movie_id", "title"]]
-    df_title.set_index('movie_id', inplace=True)
-
-    ratings = pd.merge(df_title, rating, on="movie_id")
-    df_p = ratings.pivot_table(index=['user_id'], columns=['movie_id'], values='rating').reset_index(drop=True)
-    df_p.fillna(0, inplace=True)
-
-    df_title['check'] = df_title["title"].str.lower().str.replace(" ", "")
-    df_title["check"] = df_title['check'].map(lambda x: re.sub(r'\W+', '', x))
-
-    inp = str(input("Enter Movie\n"))
-    inp = inp.lower()
-    inp = inp.replace(" ", "")
-    movie = df_title[df_title['check'].str.contains(inp)]
-    x = movie.values
-    print("The following movies matched your search")
-    print('########################################')
-    for i in range(len(x)):
-        print(str(i+1)+" "+str(x[i][0]))
-    user=int(input("\nPlease select a movie by entering its index number\n"))
-    movie_title = x[user-1][0]
-    n = int(input("how many recommendations do you want??\n"))
-    recommendations = recommend(movie_title, n)
-    print(recommendations)
